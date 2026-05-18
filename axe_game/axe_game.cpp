4fe4808ced89c33346832c27374c9d2855945837
Kofi Eshun <eshunkofi23@gmail.com>
2026-05-18 12:14:35 -0500

    Added the Axe Game

---
+++ axe_game/axe_game.cpp
@@ -0,0 +1,11 @@
+#include "raylib.h"
+
+int main()
+{
+	// Function body
+	int width;
+	width = 350;
+	// Call a function that initializes  window (takes multiple input)
+	InitWindow(width, 200, "Stephen's Window");
+	return 0;
+}
