316545d86bd94efb96b4d93e9b16e7ff9303fd31
Kofi Eshun <eshunkofi23@gmail.com>
2026-05-18 14:59:50 -0500

    Added the CLear backround nf begin and end drawing function

--- axe_game/axe_game.cpp
+++ axe_game/axe_game.cpp
@@ -1,5 +1,10 @@
 #include "raylib.h"
 
+//Numbers with fractional prt are floating-point numbers
+// Float has less precision and less memory
+// Double has more precision and more memory.
+// Boolean -true or false
+
 int main()
 {
 	// Function body
@@ -7,5 +12,17 @@ int main()
 	width = 350;
 	// Call a function that initializes  window (takes multiple input)
 	InitWindow(width, 200, "Stephen's Window");
+	float root_beer{1.99};
+	double cheese_burger{5.99};
+	bool shouldHaveLunch{true};
+	bool equal{4 == 9};
+	bool notEqual{4 != 9};
+
+	while(true){
+		BeginDrawing();
+		ClearBackground(BLUE);
+		EndDrawing();
+	}
+
 	return 0;
 }
